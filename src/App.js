35142b9c2d9f292e53cbc59b3b00028af966fb5c
Lenfumé Julien <julabina@hotmail.fr>
2022-05-13 16:31:05 +0200

    add analytics

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import CalculRentaImmo from "./Containers/CalculRentaImmo/CalculRentaImmo";
 import Cgu from "./Containers/Cgu/Cgu";
 import Contact from "./Containers/Contact/Contact";
 import NotFound from "./Containers/NotFound/NotFound";
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-W3CTME9QYY");
+ReactGA.send('pageview');
 
 function App() {
   return (
